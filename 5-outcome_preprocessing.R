750317eaa22a2b509bd8de875057d731ba6d5ebe
Mohadese SD <71869685+mohadesesd@users.noreply.github.com>
2025-02-14 16:17:59 +0330

    Update 5-outcome_preprocessing.R

--- 5-outcome_preprocessing.R
+++ 5-outcome_preprocessing.R
@@ -19,23 +19,24 @@ freadf <- function(df){return(as.data.frame(fread(df)))}
 
 # Load datasets ----------------------------------------------------------------
 df_GCST90002409 <- freadf("GCST90002409_buildGRCh38.tsv")
+print(head(df_GCST90002409))
 
 # Run MR -----------------------------------------------------------------------
 for (i in 1:22){
   # Pre-processing outcome
   ## Getting a single chromosome
-  GCST2409 <- df_GCST90002409 %>% filter(sprintf("%s", i) == df_GCST90002409$chromosome)
+  GCST2409 <- df_GCST90002409 %>% filter(sprintf("chr%s", i) == df_GCST90002409$chromosome)
   
   ### To upper case
   GCST2409$effect_allele <- toupper(GCST2409$effect_allele)
   GCST2409$other_allele  <- toupper(GCST2409$other_allele)
-  
+  print(head(GCST2409))
   ### Making SNP column
-  GCST2409$SNP <- paste("chr", GCST2409$chromosome, ":", GCST2409$base_pair_location, sep="")
+  GCST2409$SNP <- paste(GCST2409$chromosome, ":", GCST2409$base_pair_location, sep="")
   ## Merging Proxied Data
-  proxied <- readRDS('outcomeToMerge.rds')
-  proxied <- select(proxied, -SNP_Original)
-  GCST2409 <- rbind(GCST2409, proxied %>% filter(sprintf("%s", i) == proxied$chromosome))
+  #proxied <- readRDS('outcomeToMerge.rds')
+  #proxied <- select(proxied, -SNP_Original)
+  #GCST2409 <- rbind(GCST2409, proxied %>% filter(sprintf("%s", i) == proxied$chromosome))
   
   ## Getting the columns of interest
   #GCST2409_Reduced <- select(GCST2409, -sample_size)
@@ -45,9 +46,9 @@ for (i in 1:22){
 
   ### Reduced and ordered table
 
-  GCST2409_Renamed <- cbind(GCST2409[,c(1,10,5,4,8,6, 7)])
+  GCST2409_Renamed <- cbind(GCST2409[,c(1,10,5,4,8,6,7,9)])
   ### Renaming columns for harmonize_data() function
-  colnames(GCST2409_Renamed) <- c("variant_id", "SNP", "other_allele.outcome",  "effect_allele.outcome", "pval.outcome", "beta.outcome", "se.outcome")
+  colnames(GCST2409_Renamed) <- c("variant_id", "SNP", "other_allele.outcome",  "effect_allele.outcome", "pval.outcome", "beta.outcome", "se.outcome", 'samplesize.outcome')
   
   # Remove palindromic variants
   GCST2409_Renamed <- GCST2409_Renamed[
